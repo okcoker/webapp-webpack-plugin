b43e79275d294557981123c62577e6197aa2ab6c
Sean Coker <1030830+okcoker@users.noreply.github.com>
2019-04-04 17:57:24 -0400

    Add tests for `publicPath` and `outputPath`

---
+++ test/outputpathoption.test.js
@@ -0,0 +1,30 @@
+const test = require('ava');
+const path = require('path');
+const fs = require('fs-extra');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebappWebpackPlugin = require('../');
+
+const { logo, generate, mkdir, compare, expected } = require('./util');
+
+test.beforeEach(async t => t.context.root = await mkdir());
+
+test('should allow for overriding the output path of favicons', async t => {
+  const customOutputPath = 'test/path';
+  const dist = path.join(t.context.root, 'dist');
+
+  await generate({
+    context: t.context.root,
+    output: {
+      path: dist,
+      publicPath: '/public/path',
+    },
+    plugins: [
+      new HtmlWebpackPlugin(),
+      new WebappWebpackPlugin({ logo, outputPath: customOutputPath }),
+    ],
+  });
+
+  t.deepEqual(await compare(dist, path.resolve(expected, 'outputpathoption')), []);
+});
+
+test.afterEach(t => fs.remove(t.context.root));
